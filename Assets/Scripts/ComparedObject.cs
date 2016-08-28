613f343d75e47a7ae48b1024c49a7f9592b9c657
Justin Doan <soundaholic92@gmail.com>
2016-08-28 10:32:43 +0800

    Initial git

---
+++ Assets/Scripts/ComparedObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComparedObject : MonoBehaviour
+{
+    public tk2dSprite sprite;
+
+    public float size;
+    bool selected;
+
+	// Use this for initialization
+	void Start () {
+        Bounds bounds = sprite.GetUntrimmedBounds();
+        size = bounds.max.y - bounds.min.y;
+
+        sprite.gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public float selectCompare()
+    {
+        return size;
+    }
+}
